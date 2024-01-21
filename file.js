e0cde02fce7a0ca2f4fa1126b3e7cff232a3f9bb
kelvin <kelvin.musyoka@student.moringaschool.com>
2024-01-21 20:12:30 +0300

    project

---
+++ file.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded',poster)
+function poster(){
+  fetch('http://localhost:3000/shopping')
+  .then(response => response.json())
+  .then(shopping =>shopping.forEach((item)=>{
+    let container = document.getElementById('home')
+    let div=document.createElement('div');
+    div.className='product';
+    div.innerHTML = `
+    <div class = 'post'>
+      <img src=${item.source} width=300,height=500> 
+      <h2>${item.name}</h2>
+      <p>Artist: ${item.artist}</p>
+      <p>Price : ${item.price}</p>
+      <button onclick='showBoughtMessage()'>Buy</button>
+    </div>
+    
+    `
+    container.appendChild(div);
+  }))
+
+}
+function showBoughtMessage(){
+  let btn=document.querySelector('.post button')
+  btn.addEventListener('click',()=>alert('Added to cart'))
+}
